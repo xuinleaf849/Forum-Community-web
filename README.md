4be7c9c4baccbc47627ed67816844904f3a4a2ea
xuinleaf849 <xuyunye1992@gmail.com>
2018-02-19 03:38:56 -0600

    modified_1

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # README
 
-This is a good starting point for Rails projects.
+Community for MPCS student@UChicago
+=============
+
+Models:
+
+* User (Everyone who launches a discussion or posts a comment need to sign up or login.)
+* Discussion (the root discussion launched in a discussion thread)
+* Comment (comment below a specific discussion)
+* Thread (consists of one discussion and comments below it)
+
+
+How to set up:
+
+1. In your Command Prompt, cd mpcs52553-final
+2. bundle install
+3. rails db:migrate db:seed
+4. rails server
+5. Browse 'localhost:3000'.
+
+
+How to use:
+1. users need to sign up for the first time, and login if they launch a discussion or make a comment.
+Moreover, users can log out.
+2. users can launch discussions, and see, delete, comment or edit the discussions they launched.
+3. users can comment below any discussions, and see or delete their comments.
+4. users can see a whole list of threads (a thread consists of a discussion with all its following comments).
+Moreover, they can see the related threads in which they have commented or discussed.
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,47 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    root 'threads#index'
+
+    get "/users" => "users#index"
+    get "/users/new" => 'users#new'
+    post "/users" => 'users#create'
+    get "/users/:id" => 'users#show'
+    delete "/users/:id" => 'users#destroy'
+    # get "/users/:id/edit" => 'users#edit'
+    # patch "/users/:id" => 'users#update'
+
+    get "/threads" => "threads#index"
+    get "/threads/new" => 'threads#new'
+    post "/threads" => 'threads#create'
+    get "/threads/:id" => 'threads#show'
+    delete "/threads/:id" => 'threads#destroy'
+    get "/threads/:id/edit" => 'threads#edit'
+    patch "/threads/:id" => 'threads#update'
+
+    # get "/discussions" => "discussions#index"
+    get "/discussions/new" => 'discussions#new'
+    post "/discussions" => 'discussions#create'
+    get "/discussions/:id" => 'discussions#show'
+    delete "/discussions/:id" => 'discussions#destroy'
+    get "/discussions/:id/edit" => 'discussions#edit'
+    patch "/discussions/:id" => 'discussions#update'
+
+    # get "/comments" => "comments#index"
+    get "/comments/new" => 'comments#new'
+    post "/comments" => 'comments#create'
+    get "/comments/:id" => 'comments#show'
+    delete "/comments/:id" => 'comments#destroy'
+    get "/comments/:id/edit" => 'comments#edit'
+    patch "/comments/:id" => 'comments#update'
+
+
+
+    # get "/signup" => "users#new"
+
+    get "/sessions/new" => "sessions#new"
+    post "/sessions" => "sessions#create"
+    get "/logout" => "sessions#destroy"
+
+
+
 end
