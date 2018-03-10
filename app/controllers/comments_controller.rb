6c1bd130922fbe3c275864d9c40b7cfb30ccaa17
xuinleaf849 <xuyunye1992@gmail.com>
2018-03-09 21:22:50 -0600

    almost finished

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,9 @@ class CommentsController < ApplicationController
   #   comment.save
   #   redirect_to "/topics"
   # end
+  def index
+    @comments = Comment.all.limit(100).order('created_at desc')
+  end
 
   def destroy
     if session["user_id"].blank?
